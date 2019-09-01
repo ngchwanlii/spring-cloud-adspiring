54caf4589640074ad85a0d88afeec5c579c35101
ngchwanlii <ngchwanlii@gmail.com>
2019-09-01 13:10:21 -0700

    Added DConstant in dump package, dump data into a file with specific data root directory

---
+++ adspiring-ad-service/ad-common/src/main/java/com/adspiring/ad/dump/DConstant.java
@@ -0,0 +1,16 @@
+package com.adspiring.ad.dump;
+
+public class DConstant {
+
+    public static final String DATA_ROOT_DIR = "/Users/adspiring/mysql_data/";
+
+    public static final String AD_PLAN = "ad_plan.data";
+    public static final String AD_UNIT = "ad_unit.data";
+    public static final String AD_CREATIVE = "ad_creative.data";
+    public static final String AD_CREATIVE_UNIT = "ad_creative_unit.data";
+    public static final String AD_UNIT_INTEREST = "ad_unit_interest.data";
+    public static final String AD_UNIT_DISTRICT = "ad_unit_district.data";
+    public static final String AD_UNIT_KEYWORD = "ad_unit_keyword.data";
+
+
+}
