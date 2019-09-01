fea81d7ae8d0f37c65f3006c6b9ef2cbf0143cd0
ngchwanlii <ngchwanlii@gmail.com>
2019-09-01 12:31:38 -0700

    Added adunit index & object

---
+++ adspiring-ad-service/ad-search/src/main/java/com/adspiring/ad/index/adunit/AdUnitIndex.java
@@ -0,0 +1,55 @@
+package com.adspiring.ad.index.adunit;
+
+import com.adspiring.ad.index.IndexAware;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+@Component
+public class AdUnitIndex implements IndexAware<Long, AdUnitObject> {
+
+    private static Map<Long, AdUnitObject> objectMap;
+
+    static {
+        objectMap = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public AdUnitObject get(Long key) {
+        return objectMap.get(key);
+    }
+
+    @Override
+    public void add(Long key, AdUnitObject value) {
+        log.info("Before add: {}", objectMap);
+        objectMap.put(key, value);
+        log.info("After add: {}", objectMap);
+    }
+
+    @Override
+    public void update(Long key, AdUnitObject value) {
+
+        log.info("Before update: {}", objectMap);
+
+        AdUnitObject oldObject = objectMap.get(key);
+        if (oldObject == null) {
+            objectMap.put(key, value);
+        } else {
+            oldObject.update(value);
+        }
+
+        log.info("After update: {}", objectMap);
+    }
+
+    @Override
+    public void delete(Long key, AdUnitObject value) {
+
+        log.info("Before delete: {}", objectMap);
+        objectMap.remove(key);
+        log.info("After delete: {}", objectMap);
+
+    }
+}
---
+++ adspiring-ad-service/ad-search/src/main/java/com/adspiring/ad/index/adunit/AdUnitObject.java
@@ -0,0 +1,40 @@
+package com.adspiring.ad.index.adunit;
+
+import com.adspiring.ad.index.adplan.AdPlanObject;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdUnitObject {
+
+    private Long unitId;
+    private Integer unitStatus;
+    private Integer positionType;
+    private Long planId;
+
+    private AdPlanObject adPlanObject;
+
+
+    public void update(AdUnitObject newObject) {
+        if (newObject.getUnitId() != null) {
+            this.unitId = newObject.getUnitId();
+        }
+        if (newObject.getUnitStatus() != null) {
+            this.unitStatus = newObject.getUnitStatus();
+        }
+        if (newObject.getPositionType() != null) {
+            this.positionType = newObject.getPositionType();
+        }
+        if (newObject.getPlanId() != null) {
+            this.planId = newObject.getPlanId();
+        }
+        if (newObject.getAdPlanObject() != null) {
+            this.adPlanObject = newObject.getAdPlanObject();
+        }
+
+    }
+
+}
