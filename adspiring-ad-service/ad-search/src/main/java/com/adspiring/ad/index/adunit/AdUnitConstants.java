b41671f410e5df89e125af1ceeca2c3a9ef7468c
ngchwanlii <ngchwanlii@gmail.com>
2019-09-02 23:19:43 -0700

    Added AdUnitConstants

---
+++ adspiring-ad-service/ad-search/src/main/java/com/adspiring/ad/index/adunit/AdUnitConstants.java
@@ -0,0 +1,14 @@
+package com.adspiring.ad.index.adunit;
+
+public class AdUnitConstants {
+
+    public static class POSITION_TYPE {
+
+        public static final int SCREEN = 1;
+        public static final int CARD = 2;
+        public static final int CARD_MIDDLE = 4;
+        public static final int CARD_PAUSE = 8;
+        public static final int CARD_POST = 16;
+
+    }
+}
