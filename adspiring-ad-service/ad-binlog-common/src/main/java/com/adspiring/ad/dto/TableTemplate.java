8c382bf552f79a706c3ff17a50127f822e705842
ngchwanlii <ngchwanlii@gmail.com>
2019-09-04 10:05:32 -0700

    Reconstruct Ads Retrieval System with Kafka: Splitting out common constant & dto in ad-binlog-common modules

---
+++ adspiring-ad-service/ad-binlog-common/src/main/java/com/adspiring/ad/dto/TableTemplate.java
@@ -0,0 +1,27 @@
+package com.adspiring.ad.dto;
+
+import com.adspiring.ad.constant.OpType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TableTemplate {
+
+    private String tableName;
+    private String level;
+
+    private Map<OpType, List<String>> opTypeFieldSetMap = new HashMap<>();
+
+    /**
+     * Field index -> column name
+     */
+    private Map<Integer, String> posMap = new HashMap<>();
+
+}
