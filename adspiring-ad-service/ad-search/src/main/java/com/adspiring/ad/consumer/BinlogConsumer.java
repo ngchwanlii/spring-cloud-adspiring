f35c52b8c9c0ba3fb6b2760a101177e8b4639f44
ngchwanlii <ngchwanlii@gmail.com>
2019-09-04 11:08:50 -0700

    Reconstruct Ads Retrieval System: Added Kafka BinlogConsumer in ad-search module

---
+++ adspiring-ad-service/ad-search/src/main/java/com/adspiring/ad/consumer/BinlogConsumer.java
@@ -0,0 +1,42 @@
+package com.adspiring.ad.consumer;
+
+import com.adspiring.ad.dto.MySqlRowData;
+import com.adspiring.ad.sender.ISender;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Slf4j
+@Component
+public class BinlogConsumer {
+
+    private final ISender sender;
+
+    @Autowired
+    public BinlogConsumer(ISender sender) {
+        this.sender = sender;
+    }
+
+    @KafkaListener(topics = {"ad-search-mysql-data"}, groupId = "ad-search")
+    public void processMysqlRowData(ConsumerRecord<?, ?> record) {
+
+        Optional<?> kafkaMessage = Optional.ofNullable(record.value());
+        if (kafkaMessage.isPresent()) {
+            Object message = kafkaMessage.get();
+            MySqlRowData rowData = JSON.parseObject(
+                    message.toString(),
+                    MySqlRowData.class
+            );
+
+            log.info("kafka processMysqlRowData: {}", JSON.toJSONString(rowData));
+            sender.sender(rowData);
+
+        }
+    }
+
+}
