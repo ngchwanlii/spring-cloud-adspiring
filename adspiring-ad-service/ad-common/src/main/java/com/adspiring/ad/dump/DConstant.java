804ec22f7c7a7ad82b7589889d9d2fa7ce723634
ngchwanlii <ngchwanlii@gmail.com>
2019-09-05 10:04:16 -0700

    Completed ad-common module

--- adspiring-ad-service/ad-common/src/main/java/com/adspiring/ad/dump/DConstant.java
+++ adspiring-ad-service/ad-common/src/main/java/com/adspiring/ad/dump/DConstant.java
@@ -2,8 +2,7 @@ package com.adspiring.ad.dump;
 
 public class DConstant {
 
-    public static final String DATA_ROOT_DIR = "/Users/adspiring/mysql_data/";
-
+    public static final String DATA_ROOT_DIR = "/Users/jayng/Tech/spring-cloud-adspiring/mysql_data/";
     public static final String AD_PLAN = "ad_plan.data";
     public static final String AD_UNIT = "ad_unit.data";
     public static final String AD_CREATIVE = "ad_creative.data";
@@ -11,6 +10,5 @@ public class DConstant {
     public static final String AD_UNIT_INTEREST = "ad_unit_interest.data";
     public static final String AD_UNIT_DISTRICT = "ad_unit_district.data";
     public static final String AD_UNIT_KEYWORD = "ad_unit_keyword.data";
-
 
 }
